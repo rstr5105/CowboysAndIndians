53318745fc052b206300b16ca5a8efd880b157bc
Robert <rstr5105@gmail.com>
2014-07-07 20:26:13 -0700

    Fleshed Out Sprite.java, added a test unit image resource in resources/images/Units/Unit1/*.png  Hopefully, when I get around to it, animations will just work

--- CowboysAndIndians/src/game/graphicsEngine/Sprite.java
+++ CowboysAndIndians/src/game/graphicsEngine/Sprite.java
@@ -3,19 +3,70 @@ package game.graphicsEngine;
 import java.awt.Image;
 
 public class Sprite {
-
+	protected Animation anim;
+	private float x;
+	private float y;
+	
+	private float dx;
+	private float dy;
+	
+	public Sprite(Animation anim){
+		this.anim = anim;
+	}
+	
+	public void update(long elapsedTime){
+		x += dx;
+		y += dy;
+		anim.update(elapsedTime);
+	}
+	
+	
 	public Image getImage() {
-		return null;
+		return anim.getImage();
 	}
 
-	public int getX() {
+	public float getX() {
 		// TODO Auto-generated method stub
-		return 0;
+		return x;
 	}
 
-	public int getY() {
+	public float getY() {
 		// TODO Auto-generated method stub
-		return 0;
+		return y;
 	}
-
+	
+	public void setX(float x){
+		this.x = x;
+	}
+	
+	public void setY(float y){
+		this.y = y;
+	}
+	
+	public int getWidth(){
+		return anim.getImage().getWidth(null);
+	}
+	
+	public int getHeight(){
+		return anim.getImage().getHeight(null);
+	}
+	public float getVelocityX(){
+		return dx;
+	}
+	public float getVelocityY(){
+		return dy;
+	}
+	
+	public void setVelocityX(float dx){
+		this.dx = dx;
+	}
+	
+	public void setVelocityY(float dy){
+		this.dy = dy;
+	}
+	
+	public Object clone(){
+		return new Sprite(anim);
+	}
+	
 }
